0a795ecbb4f493acddd2883389e49bbc5da7b41a
Ilja <100518672+diliny867@users.noreply.github.com>
2022-05-06 17:46:23 +0300

    Camera rotation with mouse + minor changes

--- IsometricCamera/Assets/Scripts/CameraController.cs
+++ IsometricCamera/Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -8,7 +9,7 @@ public class CameraController : MonoBehaviour
     private float rotationSpeed = 80f;
     private PlayerController playerController;
     private int rotateCount=300;
-    private bool rotateByFragments = false;//rotates by a big angle in one press of a button
+    private bool rotateByFragments = true;//rotates by a big angle in one press of a button
 
     private Camera mainCamera;
 
@@ -22,10 +23,16 @@ public class CameraController : MonoBehaviour
     private Vector3 defaultPosition;
     private Quaternion defaultRotation;
     private float defaultCameraSize;
+
+    private bool rotateWithRightMouseButton = false;
+    private bool followPlayer = false;
+
+    private GameObject player;
     
     void Start()
     {
         mainCamera=Camera.main;
+        player = GameObject.FindGameObjectWithTag("Player");
         defaultCameraSize = mainCamera.orthographicSize;
         defaultPosition = transform.position;
         defaultRotation = transform.rotation;
@@ -72,31 +79,55 @@ public class CameraController : MonoBehaviour
                 mainCamera.orthographicSize += 0.5f;
             }
         }
-
-        //if (MouseMoved())
-        //{
-            //currentMousePos = Input.mousePosition;
-            //var deltaMousePos = currentMousePos - lastMousePos;
-            //lastMousePos = currentMousePos;
-        if (Input.GetMouseButton(0))//move camera
-        {
-            transform.position -= transform.right * Input.GetAxis("Mouse X") * mainCamera.orthographicSize/30f * sensitivytyX;//30f is like magic number, which i dont know how to get correctly but it almost fixes camera movement relative to its size
-            transform.position -= transform.up * Input.GetAxis("Mouse Y") * mainCamera.orthographicSize/30f * sensitivytyY;
-            //transform.Translate(deltaMousePos/cameraOffsetZ, Space.Self);
-        }
-        //}
+        
         if (Input.GetMouseButton(1))//rotate camera
         {
+            if (rotateWithRightMouseButton)//if enabled also moves the camera, when rotates
+            {
+                transform.position -= transform.right * Input.GetAxis("Mouse X") * mainCamera.orthographicSize/30f * sensitivytyX;
+                transform.position -= transform.up * Input.GetAxis("Mouse Y") * mainCamera.orthographicSize/30f * sensitivytyY;
+            }
             transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * rotationSensitivyty,Space.World);
             playerController.ModifyDefaultAngles(transform.rotation.eulerAngles.y);
         }
 
-        if (Input.GetKey(KeyCode.C))//return to default position
+        if (!followPlayer)
         {
-            transform.position = defaultPosition;
+
+            //if (MouseMoved())
+            //{
+            //currentMousePos = Input.mousePosition;
+            //var deltaMousePos = currentMousePos - lastMousePos;
+            //lastMousePos = currentMousePos;
+            if (Input.GetMouseButton(0)) //move camera
+            {
+                transform.position -= transform.right * Input.GetAxis("Mouse X") * mainCamera.orthographicSize / 30f *
+                                      sensitivytyX; //30f is like magic number, which i dont know how to get correctly but it almost fixes camera movement relative to its size
+                transform.position -= transform.up * Input.GetAxis("Mouse Y") * mainCamera.orthographicSize / 30f *
+                                      sensitivytyY;
+                //transform.Translate(deltaMousePos/cameraOffsetZ, Space.Self);
+            }
+            //}
+        }
+        else
+        {
+            transform.position = player.transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))//return to default position
+        {
+            if (!followPlayer)
+            {
+                transform.position = defaultPosition;
+            }
             transform.rotation = defaultRotation;
             mainCamera.orthographicSize = defaultCameraSize;
             playerController.ModifyDefaultAngles(transform.rotation.eulerAngles.y);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.V))//makes camera follow player
+        {
+            followPlayer = !followPlayer;
         }
     }
 
@@ -104,11 +135,12 @@ public class CameraController : MonoBehaviour
     {
         return (Input.GetAxisRaw("Mouse X")!= 0 || Input.GetAxisRaw("Mouse Y")!= 0);
     }
+    
     IEnumerator rotateCamera(int rotCount, int direction)
     {
         for (int i = 0; i < rotCount; i++)
         {
-            float timeInc = Mathf.Abs(Mathf.Cos(((180 * (i / rotCount)) * Mathf.PI) / 180));//makes it fast at start and finish, and slow at middle
+            float timeInc = Mathf.Abs(Mathf.Cos(((float)i / rotCount) * Mathf.PI));//(currenly bad.) makes it fast at start and finish, and slow at middle //Mathf.Abs(Mathf.Cos(((180 * ((float)i / rotCount)) * Mathf.PI) / 180))
             transform.Rotate(new Vector3(0, direction, 0) * timeInc * rotationSpeed * Time.deltaTime,Space.World);
             playerController.ModifyDefaultAngles(transform.rotation.eulerAngles.y);
             yield return new WaitForSeconds(0.001f);
--- IsometricCamera/Assets/Scripts/SpawnEnemies.cs
+++ IsometricCamera/Assets/Scripts/SpawnEnemies.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class SpawnEnemies : MonoBehaviour
 {
     public GameObject Enemy;
+    private float beforeSpawnDelay=1f;
+    private float spawnDelay=1f;
     void Start()
     {
-        StartCoroutine(StartSpawning());
+        StartCoroutine(StartSpawning(beforeSpawnDelay));
     }
 
     void Update()
@@ -16,13 +18,26 @@ public class SpawnEnemies : MonoBehaviour
         
     }
 
-    IEnumerator StartSpawning()
+    IEnumerator StartSpawning(float delay)
     {
+        yield return new WaitForSeconds(delay);
         while (!PlayerScript.isDead)
         {
             var pos = new Vector3(Random.Range(-15f, 15f), Enemy.transform.localScale.y/2, Random.Range(-15f, 15f));
             GameObject newEnemy = Instantiate(Enemy, pos, Quaternion.identity, transform);
-            yield return new WaitForSeconds(1f+Random.Range(-0.2f,0.2f));
+            yield return new WaitForSeconds(GetSpawnDelay(spawnDelay,-0.2f,0.2f));
+        }
+    }
+
+    float GetSpawnDelay(float delay, float rangeMinus, float rangePlus)
+    {
+        if (rangeMinus < rangePlus)
+        {
+            return spawnDelay + Random.Range(rangeMinus, rangePlus);
+        }
+        else
+        {
+            return spawnDelay;
         }
     }
 }
